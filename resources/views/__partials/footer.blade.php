96342b4a4134b03f5d175114e6eff7b3dad4c1dd
David Bastidas <espabastidas19@gmail.com>
2018-12-28 14:06:36 -0500

    fix footer

--- resources/views/__partials/footer.blade.php
+++ resources/views/__partials/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="container-fluid clearfix">
     <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
       <a href="http://www.bootstrapdash.com/" target="_blank">Gestion Avisos 2.0</a>, All rights reserved.</span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Quanta Services Colombia
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">{{config('myconfig.empresa')}}
     </span>
   </div>
 </footer>
