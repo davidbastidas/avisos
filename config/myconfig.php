c9ccff3fc65295486031e0fa433d906baaa2fc79
David <espabastidas19@gmail.com>
2019-01-27 16:09:36 -0500

    ignore myconfig

--- config/myconfig.php
+++ config/myconfig.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-  'ruta_fotos' => 'C:/wamp64/www/control/operativa/storage/app/public/fotos/',
+  'ruta_fotos' => 'C:/wamp64/www/avisos/public/fotos/',
   //'ruta_fotos' => '/var/www/html/operativa/storage/app/public/fotos/'
-  'public_fotos' => 'http://localhost/control/operativa/public/fotos/',
+  'public_fotos' => 'http://localhost/avisos/public/fotos/',
   'empresa' => 'Quanta Services Colombia',
   'mappoint' => 'cartagena',
 ];
